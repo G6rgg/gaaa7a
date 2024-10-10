4008dad175831a8f9aeda0716c02007ff169c52a
G6rgg <fordriana07@gmail.com>
2024-10-10 14:13:03 +0500

    Add files via upload

--- cpmewan.py
+++ cpmewan.py
@@ -1,8 +1,8 @@
 import requests
 from time import sleep
-BASE_URL: str = "https://maroon-echidna-796942.hostingersite.com/api"
+BASE_URL: str = "https://www.cpmewan.com/api"
 
-class CarParkTool:
+class CPMEwan:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None
