4e8273907f9d3b4cfa622676eb470bb82f050f34
G6rgg <fordriana07@gmail.com>
2024-10-17 05:44:54 +0500

    Update cpmewan.py

--- cpmewan.py
+++ cpmewan.py
@@ -2,7 +2,7 @@ import requests
 from time import sleep
 BASE_URL: str = "https://red-viper-815239.hostingersite.com/api"
 
-class G6rgg:
+class gaaa7a:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None
