a18a5ef24f9a73f2b823d0c5db87e85df62bd87e
G6rgg <fordriana07@gmail.com>
2024-10-17 07:58:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from cpmewan import CPMEwan
+from gaaa7a import G6rgg
 
 __CHANNEL_USERNAME__ = "gaaa7a"
 __GROUP_USERNAME__   = "gaaa7a_77"
